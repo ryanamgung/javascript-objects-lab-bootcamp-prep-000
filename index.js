cbd2e3e0f58e5caa2158dba949f44a63926e601d
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 05:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function updateObjectWithKeyAndValue(object, key, value)
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
-  return
+  object[key] = 'value'
+  return object
 }
 
 function deleteFromObjectByKey(object, key)
